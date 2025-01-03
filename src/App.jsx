555b3bd1e8c2d671841aa6c37555583d953f5978
Nicolas Vega <vegani44@gmail.com>
2025-01-02 21:11:04 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+
+function App() {
+  
+  const bienvenida = ' Bienvenidos a Tejidos Brunilu' 
+  return (
+    <div>
+      <h1 className='h1' >{bienvenida}</h1>
+      <p style={{color : 'greenyellow', fontWeight : 'bolder'}}>
+        Holaaaa😊
+      </p>
+    </div>
+  )
+}
+
+export default App
